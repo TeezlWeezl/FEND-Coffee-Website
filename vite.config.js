758f8acfa5d3d4ef5c00ebc7ce041624ad803466
Thies Oelke <thies.oelke@gmail.com>
2023-02-10 17:25:49 +0100

    Adds skeleton of subpage "Herkunft"

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,8 @@ module.exports = defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
-        nested: resolve(__dirname, 'src/das-cafe/index.html')
+        nested: resolve(__dirname, 'src/das-cafe/index.html'),
+        nested: resolve(__dirname, 'src/herkunft/index.html')
       }
     }
   },
