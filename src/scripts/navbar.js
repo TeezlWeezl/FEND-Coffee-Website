8f7357d3d310d53ba7aa80a1e9f838ddf2747af3
Thies Oelke <thies.oelke@gmail.com>
2023-02-24 17:36:23 +0100

    adds styling for the mobile navigation menu, failed to implement css transition

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -4,8 +4,10 @@ const toggleMobileNavbar = () => {
   console.log(navLinks[0].style.display);
   let toggleState = navLinks[0].style.display === 'none'
   if (toggleState) {
-    navLinks[0].style.display = 'block'
+    navLinks[0].style.display = 'flex'
+    navLinks[0].classList.add('nav-links--open')
   } else {
     navLinks[0].style.display = 'none'
+    navLinks[0].classList.remove('nav-links--open')
   }
 }
