b69caed64904c86e97aadc9d3648600d3cf4e518
Thies Oelke <thies.oelke@gmail.com>
2023-02-28 23:06:44 +0100

    Adds transition for mobile view when onclick of burger menu

--- src/notes.md
+++ src/notes.md
@@ -1,9 +1,4 @@
 ## TO DO
 ------------
-
-- Häufig verwendete Komponenten durch Handlebars ersetzen
- - (opt) Buttons 
- - (opt) Adresse & Öffnungszeiten
-
-- Media Queries durch genestede Selektoren stylen (SubPage: Das Café)
 - (opt) Startseite durch genestete Media Queries Updaten
+- (opt) Restylen der Bildergalerie nach dem Muster der Videoaufzeichnung vom 27.02.23
--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -1,13 +1,10 @@
 const toggleMobileNavbar = () => {
-  console.log('Script is running');
   const navLinks = document.getElementsByClassName('nav-links')
-  console.log(navLinks[0].style.display);
-  let toggleState = navLinks[0].style.display === 'none'
+  let toggleState = navLinks[0].classList.contains !== 'nav-links--open'
   if (toggleState) {
-    navLinks[0].style.display = 'flex'
-    navLinks[0].classList.add('nav-links--open')
+    navLinks[0].style.transition = 'background-color 2s ease-in;'
+    navLinks[0].classList.toggle('nav-links--open')
   } else {
-    navLinks[0].style.display = 'none'
     navLinks[0].classList.remove('nav-links--open')
   }
 }
