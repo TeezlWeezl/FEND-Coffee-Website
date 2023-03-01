23b63d06578bbaea7b843c8f37015d88e2585fde
Thies Oelke <thies.oelke@gmail.com>
2023-03-01 20:00:05 +0100

    adds event listeners for navbar via JS in order for vite to be able to bundle it

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -1,10 +1,14 @@
-const toggleMobileNavbar = () => {
-  const navLinks = document.getElementsByClassName('nav-links')
-  let toggleState = navLinks[0].classList.contains !== 'nav-links--open'
-  if (toggleState) {
-    navLinks[0].style.transition = 'background-color 2s ease-in;'
-    navLinks[0].classList.toggle('nav-links--open')
-  } else {
-    navLinks[0].classList.remove('nav-links--open')
-  }
-}
+const navLinks = document.getElementById('nav-links')
+const burgerMenu = document.getElementsByClassName('burger-menu-action')
+
+Object.entries(burgerMenu).forEach(([key, element]) => {
+  element.addEventListener('click', () => {
+    let toggleState = navLinks.classList.contains !== 'nav-links--open'
+    if (toggleState) {
+      navLinks.classList.toggle('nav-links--open')
+    } else {
+      navLinks.classList.remove('nav-links--open')
+    }
+  })
+});
+
