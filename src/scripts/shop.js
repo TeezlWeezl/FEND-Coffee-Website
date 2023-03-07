e08d8e85a2989dcd553410d51ea277ec16a4ae3c
Thies Oelke <thies.oelke@gmail.com>
2023-03-07 19:24:56 +0100

    Shop: Adds selection for different sizes when quick-add clicked

--- src/scripts/shop.js
+++ src/scripts/shop.js
@@ -1,7 +1,17 @@
 const quickAdd = document.querySelectorAll('.quick-add')
 quickAdd.forEach(el => {
+  const productImage = el.previousElementSibling
+  const specSelectButtonWrapper = el.nextElementSibling
   el.addEventListener('click', (e) => {
-    console.log(e.target.classList);
-    e.target.classList.toggle('quick-add--clicked')
+    el.classList.add('quick-add--clicked')
+    specSelectButtonWrapper.classList.add('spec-select--active')
+  })
+  productImage.addEventListener('mouseover', e => {
+    el.classList.toggle('quick-add--active')
+  })
+  productImage.addEventListener('mouseout', (e) => {
+    el.classList.remove('quick-add--active')
+    el.classList.remove('quick-add--clicked')
+    specSelectButtonWrapper.classList.remove('spec-select--active')
   })
 })
