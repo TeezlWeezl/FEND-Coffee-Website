8f53a2a6f84b883c7ffb4252d525376bbe054b22
Thies Oelke <thies.oelke@gmail.com>
2023-03-10 13:04:49 +0100

    adds quick-add buttons for shop page

--- src/scripts/shop.js
+++ src/scripts/shop.js
@@ -1,17 +1,37 @@
-const quickAdd = document.querySelectorAll('.quick-add')
-quickAdd.forEach(el => {
-  const productImage = el.previousElementSibling
-  const specSelectButtonWrapper = el.nextElementSibling
-  el.addEventListener('click', (e) => {
-    el.classList.add('quick-add--clicked')
-    specSelectButtonWrapper.classList.add('spec-select--active')
+// If screen is desktopSize enable quick add buttons
+if (matchMedia('(min-width: 992px)').matches) {
+  const productAction = document.querySelectorAll('.product-action')
+
+  productAction.forEach(el => {
+    const productImg = el.previousElementSibling
+    const quickAdd = el.children[0]
+    const specSelect = el.children[1]
+
+    productImg.addEventListener('mouseenter', (e) => {
+      el.classList.remove('product-action--hidden')
+    })
+
+    productImg.addEventListener('mouseout', (e) => {
+      el.classList.add('product-action--hidden')
+
+      // Resets quick add and specSelect
+      quickAdd.classList.remove('product-action__quick-add--hidden')
+      specSelect.classList.add('product-action__spec-select--hidden')
+    })
+
+    el.addEventListener('mouseover', (e) => {
+      el.classList.remove('product-action--hidden')
+    })
+
+    quickAdd.addEventListener('click', (e) => {
+      quickAdd.classList.add('product-action__quick-add--hidden')
+      specSelect.classList.remove('product-action__spec-select--hidden')
+    })
+
+    for (const el of specSelect.children) {
+      el.addEventListener('click', (e) => {
+        specSelect.classList.add('product-action__spec-select--hidden')
+      })
+    }
   })
-  productImage.addEventListener('mouseover', e => {
-    el.classList.toggle('quick-add--active')
-  })
-  productImage.addEventListener('mouseout', (e) => {
-    el.classList.remove('quick-add--active')
-    el.classList.remove('quick-add--clicked')
-    specSelectButtonWrapper.classList.remove('spec-select--active')
-  })
-})
+}
