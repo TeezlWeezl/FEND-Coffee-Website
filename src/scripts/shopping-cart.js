99447b4b0148150629a2101155448d32d2d98908
Thies Oelke <thies.oelke@gmail.com>
2023-03-15 20:56:47 +0100

    adds remove function to shopping cart when button is clicked

--- src/scripts/shopping-cart.js
+++ src/scripts/shopping-cart.js
@@ -31,41 +31,44 @@ if (!localStorage.getItem('products')) {
   localStorage.setItem('products', JSON.stringify([]))
 }
 
+// Getting items back from localStorage
+const productsGet = JSON.parse(localStorage.getItem('products'))
+const productCounter = document.getElementById('product-counter')
+const productsContainer = document.getElementById('products')
+const subtotalEl = document.getElementById('subtotal')
+const deliveryCostsEl = document.getElementById('delivery-costs')
+const totalEl = document.getElementById('total')
+
 export default function renderCart() {
-  // Getting items back from localStorage
-  const productsGet = JSON.parse(localStorage.getItem('products'))
-  const productCounter = document.getElementById('product-counter')
-  const productsContainer = document.getElementById('products')
   let productsHTML = ''
   let subtotal = 0
   let deliveryCosts = 0
-  const subtotalEl = document.getElementById('subtotal')
-  const deliveryCostsEl = document.getElementById('delivery-costs')
-  const totalEl = document.getElementById('total')
 
   // Setting the value of the Product counter
   productCounter.innerText = `${productsGet.length} Produkte`
 
   // Setting up the Products 
-  for (const item of productsGet) {
+  productsGet.forEach((item, i) => {
+
     subtotal += item.price
     productsHTML += `
     <div class="cart__product">
-    <div class="product__img">
-      <img src="/images/FEND_Coffee_Costa-Rica 2.webp" alt="">
+      <div class="product__img">
+        <img src="/images/FEND_Coffee_Costa-Rica 2.webp" alt="">
+      </div>
+      <div class="product__specs">
+        <h3 class="specs__header">${item.prodName}</h3>
+        <p class="specs__grind-info">${item.grind ? 'gemahlen' : 'Bohnen'}</p>
+        <p class="specs__size-info">${item.size}</p>
+        <p class="specs__delivery-info">sofort versandbereit</p>
+        <p class="specs__price price">${(item.price / 100).toFixed(2).toString().replace('.', ',')}</p>
+        <button class="specs__remove-item" cart-position=${i}><img src="/images/icons/burger-menu-close.svg"></button>
+      </div>
     </div>
-    <div class="product__specs">
-      <h3 class="specs__header">${item.prodName}</h3>
-      <p class="specs__grind-info">${item.grind ? 'gemahlen' : 'Bohnen'}</p>
-      <p class="specs__size-info">${item.size}</p>
-      <p class="specs__delivery-info">sofort versandbereit</p>
-      <p class="specs__price price">${(item.price / 100).toFixed(2).toString().replace('.', ',')}</p>
-      <button class="specs__remove-item">X</button>
-    </div>
-  </div>
   `
-  }
+  })
   productsContainer.innerHTML = productsHTML
+
 
   // Setting values for the order-overview
   subtotalEl.innerText = (subtotal / 100).toFixed(2).toString().replace('.', ',')
@@ -83,4 +86,17 @@ export default function renderCart() {
 
   deliveryCostsEl.innerText = (deliveryCosts / 100).toFixed(2).toString().replace('.', ',')
   totalEl.innerText = ((subtotal + deliveryCosts) / 100).toFixed(2).toString().replace('.', ',')
+
+  registerRemoveButtons()
+}
+
+const registerRemoveButtons = (productsList) => {
+  for (const btn of document.querySelectorAll('.specs__remove-item')) {
+    btn.addEventListener('click', (e) => {
+      const cartIndex = btn.getAttribute('cart-position')
+      productsGet.splice(parseInt(cartIndex), 1)
+      localStorage.setItem('products', productsGet)
+      renderCart()
+    })
+  }
 }
