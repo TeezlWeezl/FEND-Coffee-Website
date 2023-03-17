ae79f591926d6f3f4d68b8513e96f3b0cb0fdbe9
Thies Oelke <thies.oelke@gmail.com>
2023-03-17 11:07:03 +0100

    quick add buttons append product obj to localstorage when clicked

--- src/scripts/shop.js
+++ src/scripts/shop.js
@@ -7,6 +7,22 @@ if (matchMedia('(min-width: 992px)').matches) {
     const quickAdd = el.children[0]
     const specSelect = el.children[1]
 
+    // Declaration of product obj
+    const addProduct = {
+      prodID: parseInt(specSelect.getAttribute('product-id')),
+      prodName: '',
+      grind: '',
+      size: '',
+      price: '',
+    }
+
+    // Simuliert Datenbankabfrage
+    if (addProduct.prodID === 100) {
+      addProduct.prodName = 'Costa Rica'
+      addProduct.price = 2690
+      addProduct.grind = true
+    }
+
     productImg.addEventListener('mouseenter', (e) => {
       el.classList.remove('product-action--hidden')
     })
@@ -28,8 +44,20 @@ if (matchMedia('(min-width: 992px)').matches) {
       specSelect.classList.remove('product-action__spec-select--hidden')
     })
 
+    console.log(specSelect.children);
+
     for (const el of specSelect.children) {
       el.addEventListener('click', (e) => {
+        // Set the size of the product that should be added to the cart
+        addProduct.size = parseInt(e.target.getAttribute('product-size'))
+
+        // fetch product cart data from localStorage and append addProduct at the end
+        let currentCart = JSON.parse(localStorage.getItem('products'))
+        console.log(typeof currentCart, currentCart);
+        currentCart.push(addProduct)
+        localStorage.setItem('products', JSON.stringify(currentCart))
+
+        // Hide buttons after clicked
         specSelect.classList.add('product-action__spec-select--hidden')
       })
     }
